65f6fc2e0f21eecf72f9adbbb7a60081290a7ed8
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-04-14 13:19:42 +0600

    complete responsiveness of top nav

--- client/src/components/PagesTotleSection.js
+++ client/src/components/PagesTotleSection.js
@@ -4,14 +4,14 @@ import { NavLink } from 'react-router-dom'
 
 export default function PagesTotleSection() {
   return (
-    <div className='w-full flex my-2'>
-        <div className='w-6/12'>
+    <div title='New Admission' className='w-full flex my-2'>
+        <div className='w-9/12 md:w-6/12'>
             <h1 className='text-2xl font-bold'>Dashboard</h1>
-            <p className='text-gray-400 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p title='Lorem ipsum dolor sit amet consectetur adipisicing elit.' className='text-gray-400 text-xs truncate '>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
-        <div className='w-6/12 flex justify-end'>
+        <div className='w-3/12 md:w-6/12 flex justify-end'>
             <NavLink className={'flex items-center bg-grad-to h-11 px-4 rounded-lg text-white'} to={'/'}>
-                <BiPlus /> New Admission
+                <BiPlus /> <span className='hidden md:block'> New Admission</span>
             </NavLink>
         </div>
     </div>
